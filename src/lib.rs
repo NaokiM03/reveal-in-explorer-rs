5879e95e1d0b771ac017c45a4960613551cf9da7
NaokiM03 <naokim03.lib+dev@gmail.com>
2024-09-04 17:55:44 +0900

    first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,42 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+use std::{io, path::Path};
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+use windows::{
+    core::HSTRING,
+    Win32::{
+        System::Com::{CoInitializeEx, CoUninitialize, COINIT_MULTITHREADED},
+        UI::Shell::{Common::ITEMIDLIST, ILCreateFromPathW, ILFree, SHOpenFolderAndSelectItems},
+    },
+};
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+pub fn reveal_in_explorer(path: impl AsRef<Path>) -> io::Result<()> {
+    unsafe {
+        let pvreserved = None;
+        let dwcoinit = COINIT_MULTITHREADED;
+        CoInitializeEx(pvreserved, dwcoinit).ok()?;
+
+        let pidlfolder: *const ITEMIDLIST = {
+            let path = path.as_ref();
+            let pszpath = HSTRING::from(path);
+            ILCreateFromPathW(&pszpath)
+        };
+
+        if pidlfolder.is_null() {
+            return Err(io::Error::last_os_error());
+        }
+
+        let result = {
+            let apidl = None;
+            let dwflags = 0;
+            SHOpenFolderAndSelectItems(pidlfolder, apidl, dwflags)
+        };
+
+        let pidl = Some(pidlfolder);
+        ILFree(pidl);
+
+        CoUninitialize();
+
+        result?;
     }
+
+    Ok(())
 }
